a8e96648940383d24184009cf060ac5564be200e
Kaiwanyawit Chawankul <kaiwanyawit.c@thoughtworks.com>
2021-11-26 21:23:30 +0700

    try again

--- lighthouserc.js
+++ lighthouserc.js
@@ -1,13 +1,13 @@
 module.exports = {
     ci: {
       collect: {
-        //url:[
-        //  "https://localhost:5100/",
-        //  "https://localhost:5100/counter",
-        //  "https://localhost:5100/fetchdata"
-        //],
-        staticDistDir: "./publish",
-        //startServerCommand: "dotnet run --project src/test-lighthouse-ci.csproj --urls https://localhost:5100  --no-build --verbosity normal",
+        url:[
+          "https://localhost:PORT/",
+          "https://localhost:PORT/counter",
+          "https://localhost:PORT/fetchdata"
+        ],
+        //staticDistDir: "./publish",
+        startServerCommand: "dotnet run --project src/test-lighthouse-ci.csproj --urls https://localhost:5100  --no-build --verbosity normal",
         numberOfRuns: 1,
         startServerReadyTimeout: 90000,
       },
